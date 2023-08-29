232f1db18663ccbce0744a8f969267fa96b2cd00
rutviklhase <rutvik.lhase@genmills.com>
2023-08-30 01:02:13 +0530

    Added instructions to 206

--- 206 - Reverse Linked List.py
+++ 206 - Reverse Linked List.py
@@ -19,11 +19,11 @@ def main():
     ln3 = ListNode(3, None)
     ln2 = ListNode(2,ln3)
     ln = ListNode(1,ln2)
+    #Add custom ListNodes here 
     
     soln = Solution()
-    ans = soln.reverseList(ln)
-    
-    soln.printList(ans)
+    ans = soln.reverseList(ln) #Default arguments passed - [1,2,3]
+    soln.printList(ans) #Expected out with with Default arguments - [3,2,1]
     
 if __name__ == "__main__":
     main()
