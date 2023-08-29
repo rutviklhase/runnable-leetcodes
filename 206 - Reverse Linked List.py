93f3d557d7435139673a95e9934b6c77576eb4af
rutviklhase <rutvik.lhase@genmills.com>
2023-08-30 00:52:31 +0530

    206

---
+++ 206 - Reverse Linked List.py
@@ -0,0 +1,41 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+        
+#Solution on Leetcode starts here -
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        if not head:
+            return None
+        
+        newHead = head
+        
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+            
+        head.next = None
+        return newHead
+    
+    def printList(self, head: ListNode) -> ListNode:
+        
+        print(head.val)
+        
+        if head.next:
+            newHead = self.printList(head.next)
+        
+def main():    
+    ln3 = ListNode(3, None)
+    ln2 = ListNode(2,ln3)
+    ln = ListNode(1,ln2)
+    
+    soln = Solution()
+    ans = soln.reverseList(ln)
+    
+    soln.printList(ans)
+    
+if __name__ == "__main__":
+    main()
+        
